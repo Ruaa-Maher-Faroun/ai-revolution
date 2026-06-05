5ba737cc73ca7d16a69906e3f05c694c3344aa4a
ruaa <Roaa.maher.faroun@gmail.com>
2026-06-05 20:25:08 +0300

    added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <section className='app-container'>
+      <div className='size-50 border-8 border-slate-400 border-t-primary animate'></div>
+    </section>
+  )
+}
+
+export default Loader
