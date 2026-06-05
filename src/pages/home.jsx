1bc8a90e41ef0759a608b060f3d7f1736fec82ae
Rua'a Maher Faroun <110242306+Ruaa-Maher-Faroun@users.noreply.github.com>
2026-06-05 23:37:55 +0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AITypes from '../components/AITypes';
 import AIBenefits from '../components/AIBenefits';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-import Loader from '../components/Loader';
+// import Loader from '../components/Loader';
 
 const Home = () => {
-  const [loading,setLoading] = useState(true);
+  // const [loading,setLoading] = useState(true);
 
-  useEffect(() => {
-  const timer = setTimeout(() => setLoading(false), 1000);
-  return () => clearTimeout(timer); // cleanup if component unmounts
-}, []); 
-  if(loading) return <Loader />
+//   useEffect(() => {
+//   const timer = setTimeout(() => setLoading(false), 1000);
+//   return () => clearTimeout(timer); // cleanup if component unmounts
+// }, []); 
+//   if(loading) return <Loader />
 
   return (
     <>
